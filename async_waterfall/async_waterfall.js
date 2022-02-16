734417e00902027e71c6db20e41b45a3564fbf47
Amiya955 <t-amiya.ranjan@ocltp.com>
2022-02-16 23:32:16 +0530

    Second Commit

--- async_waterfall/async_waterfall.js
+++ async_waterfall/async_waterfall.js
@@ -1,1 +1,21 @@
 console.log("Entering into code!");
+
+const async=require("async");
+
+async.waterfall([
+    (done)=>{
+        done(null,'Value 1');
+    },
+    (value1,done)=>{
+        console.log(value1);
+        done(null,'Value 2');
+    },
+    (value2,done)=>{
+        console.log(value2);
+        done(null,'done');
+    }
+],(err)=>{
+    if(err){
+        throw new Error(err);
+    }
+});

b8d2e570211eaec206a5c6f62126ba176223d25d
Amiya955 <t-amiya.ranjan@ocltp.com>
2022-02-17 21:56:31 +0530

    Modified reduce.js

--- arrays/reduce.js
+++ arrays/reduce.js
@@ -1,7 +1,7 @@
 let array=[1,2,3,4,5,6];
 
 function reducer(previous, current, index, array) {
-  let returns=previous+current;
+  const returns=previous+current;
   console.log(`previous: ${previous}, current: ${current}, index: ${index}`);
   return returns;
 }
